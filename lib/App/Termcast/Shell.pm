71290e0b5c8a86a77071107d2e405065aed4587d
Jason May <jason.a.may@gmail.com>
2011-09-24 21:36:18 -0400

    First sketch of the termcast-shell app
    So far, just dispatches for a list of active streams. Can't connect or
    or watch streams yet.

---
+++ lib/App/Termcast/Shell.pm
@@ -0,0 +1,119 @@
+package App::Termcast::Shell;
+use Moose;
+use Term::ReadLine;
+
+use JSON;
+use Cwd;
+
+use IO::Socket::UNIX;
+use Time::Duration;
+
+with 'MooseX::Getopt';
+
+has output_sub => (
+    is      => 'ro',
+    isa     => 'CodeRef',
+    traits  => ['NoGetopt'],
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        return sub {
+            my $msg = shift;
+            print {$self->term->OUT} $msg;
+        };
+    },
+);
+
+has socket => (
+    is            => 'ro',
+    isa           => 'Str',
+    documentation => 'Path to the UNIX socket that apps connect to',
+    required      => 1,
+);
+
+has prompt_sub => (
+    is      => 'ro',
+    isa     => 'CodeRef',
+    traits  => ['NoGetopt'],
+    default => sub {
+        return sub {
+            return '> ';
+        };
+    },
+);
+
+has term => (
+    is => 'ro',
+    default => sub { Term::ReadLine->new('Termcast Shell') },
+);
+
+sub output {
+    my $self = shift;
+    $self->output_sub->(@_);
+}
+
+sub say {
+    my $self = shift;
+    my $message = shift;
+    $self->output("$message\n");
+}
+
+sub run {
+    my $self = shift;
+
+    my %dispatch = (
+        list => sub {
+            my @args = @_;
+            my $json = JSON->new;
+            my $socket = IO::Socket::UNIX->new(
+                Peer => Cwd::abs_path($self->socket),
+            );
+
+            $socket->syswrite(JSON::encode_json({request => 'sessions'}));
+
+            my ($data, $buf);
+            {
+                $socket->sysread($buf, 4096) until $buf;
+                ($data) = $json->incr_parse($buf);
+                redo until $data;
+            }
+            my $output = '';
+            if ($data->{response} eq 'sessions') {
+                my @sessions = @{ $data->{sessions} };
+                for my $session (@sessions) {
+                    my $ago = ago(time() - $session->{last_active});
+                    $output .= sprintf(
+                        "[%s] %s (%sx%s) - active %s\n",
+                        $session->{session_id},
+                        $session->{user},
+                        @{ $session->{geometry} },
+                        $ago,
+                    );
+                }
+            }
+            $output ||= 'Nobody is currently streaming.';
+            $self->say($output);
+            $socket->close;
+        },
+    );
+
+    $self->prompt_sub->();
+    while (my $output = $self->term->readline('termcast> ')){
+        chomp $output;
+        my @words = split ' ', $output;
+        my $command = shift @words;
+
+        my $sub = $dispatch{$command} || sub {
+            $self->say("Unknown command.");
+        };
+
+        $sub->(@words);
+
+        $self->prompt_sub->();
+    }
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
